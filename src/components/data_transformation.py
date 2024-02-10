b2691dfa053bcec472d97696a85a3f9b84867730
shariquemd <mdsharique.jmi@gmail.com>
2024-02-10 15:00:16 +0530

    Hyperparameter Training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,19 +1,22 @@
 import sys
 from dataclasses import dataclass
-import numpy as np
+
+import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
 from src.exception import CustomException
 from src.logger import logging
 import os
+
 from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -119,7 +122,5 @@ class DataTransformation:
             )
         except Exception as e:
             raise CustomException(e,sys)
-
-        
             
     
